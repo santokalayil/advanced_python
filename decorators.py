bb48a83341266c4e6bfdd9a650757f09f769c950
Santo K Thomas <santokalayil@Santos-MacBook-Air.local>
2024-07-07 16:00:56 +0530

    decorators along with typehinting is added

---
+++ decorators.py
@@ -0,0 +1,32 @@
+from typing import Callable, ParamSpec, TypeVar
+from functools import wraps
+
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+def log(level: str = "INFO") -> Callable[[Callable[P, R]], Callable[P, R]]:
+    def decorator(fn: Callable[P, R]) -> Callable[P, R]:
+        @wraps(fn)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+            print(f"The function/method '{fn.__name__}' started")
+            print(f"{level=}")
+            try:
+                return fn(*args, **kwargs)
+            finally:
+                print(f"The function/method '{fn.__name__}' is completed")
+        
+        return wrapper
+    return decorator
+
+@log("DEBUG")
+def do_something(name: str):
+    print(f"Doing something with {name=}")
+
+
+do_something("Santo")
+
+
+
+

e2c0f43ee6448be7d025a5b5813d02b6e85e84d7
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2025-07-16 10:42:31 +0530

    Update generators.py

--- generators.py
+++ generators.py
@@ -35,3 +35,50 @@ if st.button(f"Executing Process"):
     
 if returned_out != dummy_out:
     st.info(f"OUTPUT RETURNED IS {returned_out}")
+
+
+# ============================ IMPORTANT KNOWLEDGE ==========================
+"""
+Generator Type Hinting + how it works + how yield, send and return can be used
+to make sure full potential of python is being used
+"""
+from typing import Generator
+
+YieldType = str
+SendType = str
+ReturnType = str
+
+def generator() -> Generator[YieldType, SendType, ReturnType]:
+    result1 = yield "santo"
+    print(result1, "is yielded")
+    result2 = yield "sajan"
+    print(result1, result2, "are yielded")
+    result3 = yield "saly"
+    print(result1, result2, result3, "are yielded")
+    return "complete"
+
+gen = generator()
+
+value_yielded = next(gen)
+print(f"Received (value_yielded) BEFORE while loop")
+while True:
+    try:
+        value_yielded = gen.send(value_yielded)
+        print (f"Received (value yielded) inside while loop")
+    except StopIteration as e:
+        print("StopIteration occured")
+        print(f"RETURNED the value {e.value}")
+        break
+
+"""OUTPUT
+
+Received (value_yielded) BEFORE while loop
+santo is yielded
+Received (value yielded) inside while loop
+santo sajan are yielded
+Received (value yielded) inside while loop
+santo sajan saly are yielded
+StopIteration occured
+RETURNED the value complete
+
+"""
